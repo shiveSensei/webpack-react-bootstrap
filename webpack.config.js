94eddec3a880775981dda8f3f10f1d26927fba23
shiveSensei <afroninjah@me.com>
2019-04-02 08:13:09 -0500

    added clientApp folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,69 +4,66 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index_bundle.js"
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                styles: {
-                    name: "styles",
-                    test: /\.css$/,
-                    chunks: "all",
-                    enforce: true
-                }
-            }
+  entry: "./ClientApp/src/index.js",
+  output: {
+    path: path.join(__dirname, "/ClientApp/dist"),
+    filename: "index_bundle.js"
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true
         }
-    },
-    module: {
-        rules: [
-            //bable compiler rule, check for any js, jsx files
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            //rule for loading stylesheets, and applying css styles
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: devMode
-                            ? "style-loader"
-                            : MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
-                            // importLoaders: 1
-                        }
-                    },
-                    "sass-loader"
-                ]
-            },
-            //rule for loading images
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
-            },
-           
+      }
+    }
+  },
+  module: {
+    rules: [
+      //bable compiler rule, check for any js, jsx files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      //rule for loading stylesheets, and applying css styles
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+              // importLoaders: 1
+            }
+          },
+          "sass-loader"
         ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: devMode ? "[name].css" : "[name].[hash].css",
-            chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
-        })
+      },
+      //rule for loading images
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
     ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./ClientApp/public/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ]
 };
